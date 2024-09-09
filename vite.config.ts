e1c30c32ac8fb6e260e2e685f083f562a2690fbf
ace3366 <poohmandu22@gmail.com>
2024-09-09 14:19:36 +0700

    fix:small change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    __APP_ENV__: JSON.stringify(process.env.VITE_VERCEL_ENV),
+    __APP_ENV__: JSON.stringify(process.env.VITE_API_URL),
   },
   plugins: [react()],
   resolve: {
